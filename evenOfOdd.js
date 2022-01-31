dbadeead7e0f81fe5fd826b58aa0337d599c5fd6
sanskrati-1234 <55913586+sanskrati-1234@users.noreply.github.com>
2022-01-31 08:40:56 +0530

    Update evenOfOdd.js

--- evenOfOdd.js
+++ evenOfOdd.js
@@ -1,7 +1,5 @@
 function evenAndOdd(n){
-    if(n==0){
-        console.log(n);
-    }else if(!Boolean(n)|| isNaN(n)){
+    if(n!==0 && !Boolean(n)|| isNaN(n)){
         console.log("its falsy value");
     }else if(n%2==0){
             console.log("even number");
